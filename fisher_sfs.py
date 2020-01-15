6303968939f45f06bbe7f08c914b4b95d30da332
szymow <39216239+szymow@users.noreply.github.com>
2020-01-15 18:19:24 +0100

    Działa - Fisher 3 cechy - SFS 8 cech

--- fisher_sfs.py
+++ fisher_sfs.py
@@ -27,10 +27,52 @@ def getCSV():
     sciezkaDoPliku = filedialog.askopenfilename()
     dane = pd.read_csv(sciezkaDoPliku, sep = ',')
     print(dane)
+    
+def start():
+    global fisher_lub_sfs
+    fisher_lub_sfs = v.get()
+    if fisher_lub_sfs == 1:
+        print("Fisher")
+        global lcf # Liczba cech Fisher
+        lcf = combo1.get()
+        lcf = int(lcf)
+        print("Liczba cech: ", lcf)
+    elif fisher_lub_sfs == 2:
+        print("SFS")
+        global liczbacech #Zadana liczba najlepszych cech (SFS)
+        liczbacech = combo2.get()
+        liczbacech = int(liczbacech)
+        print("Liczba cech: ", liczbacech)
+    else:
+        print("Wybierz Fisher lub SFS")
+        
 
 przyciskImportujCSV = tk.Button(text=" Importuj CSV ", 
                              command=getCSV, bg='green', fg='white', font=('helvetica', 12, 'bold'))
 przyciskImportujCSV.grid(column=1, row=0)
+
+start = tk.Button(text=" Start ", 
+                             command=start, bg='green', fg='white', font=('helvetica', 12, 'bold'))
+start.grid(column=1, row=3)
+
+
+v = tk.IntVar()
+
+r1 = tk.Radiobutton(okno, text="Fisher", font=("Arial",16), variable=v, value=1)
+r2 = tk.Radiobutton(okno, text="SFS", font=("Arial",16), variable=v, value=2)
+
+r1.grid(column = 0, row = 1)
+r2.grid(column = 0, row = 2)
+
+combo1 = Combobox(okno)
+combo1['values']=(1,2,3)
+combo1.current(1)
+combo1.grid(column=1,row=1)
+
+combo2 = Combobox(okno)
+combo2['values']=(1,2,3,4,5,6,7,8)
+combo2.current(6)
+combo2.grid(column=1,row=2)
 
 okno.mainloop()
 #https://likegeeks.com/python-gui-examples-tkinter-tutorial/
@@ -46,7 +88,6 @@ klasa_Quercus = klasa_Quercus.drop(columns=["64"])
 #Srednia cech
 srednia_Acer = klasa_Acer.mean(axis = 0)
 srednia_Quercus = klasa_Quercus.mean(axis = 0)
-
 
 def odchylenie_std(klasa, srednia):
     odchylenie = []
@@ -76,9 +117,7 @@ for i in range(int(len(srednia_Acer))):
 najlepszaCecha = wspolczynniki.index(max(wspolczynniki))
 print("najlepszaCecha Fisher dla jednej cechy to: ",najlepszaCecha)
 
-from itertools import product
-
-liczbacech = 8 #Zadana liczba najlepszych cech
+from itertools import product, permutations
 
 #Transponowanie
 A = klasa_Acer.T
@@ -97,43 +136,40 @@ nB = np.size(B,1)
 DZIELNIK_A = 1/nA
 DZIELNIK_B = 1/nB
 
-a = range(najlepszaCecha,najlepszaCecha+1)
-b = range(64)
-prod = product(a,b) #Produkt kartezjanski
-
-from time import perf_counter
-t1_start = perf_counter() 
-
-for lc in range(2,liczbacech+1):
-     
+def Fisher():
+    permutacja = list(permutations(np.arange(nX), lcf))
+    permutacja = np.array(permutacja)
+    
     maks = 0
     
-    for iterator in prod:
-        print (iterator)
-        
-        #Pomijaj gdy numer cech sie powtarzaja
-        if len(iterator) != len(set(iterator)):
-            continue
-        
+    from time import perf_counter
+    t1_start = perf_counter() 
+    
+    for iterator in permutacja:
+        #Sledzenie postepu dla 2 najlepszych cech
+        if lcf==2:
+            if iterator[0]==(iterator[1]-1):
+                print (iterator[0],"/",nX)
+        if lcf==3:
+            if iterator[0]==(iterator[1]-1)==(iterator[2]-2):
+                print (iterator[0],"/",nX)
         licznik = 0
-        
         macA = []
         macB = []
-
+    
         for i in iterator:
             licznik = licznik + (pow(sredniaA[i] - sredniaB[i],2))
             for j in range(176):
                 macA.append(A[i][j] - sredniaA[i])
             for j in range(608):
                 macB.append(B[i][j] - sredniaB[i])
-            licznik = math.sqrt(licznik)
-        
+        licznik = math.sqrt(licznik)
+            
         macA = np.array(macA)
         macB = np.array(macB)
-        
-        macA = macA.reshape(lc,176)
-        macB = macB.reshape(lc,608)
-        
+            
+        macA = macA.reshape(lcf,176)
+        macB = macB.reshape(lcf,608)
         transA = macA.T
         resultA =  np.mat(macA) * np.mat(transA)
         
@@ -154,53 +190,130 @@ for lc in range(2,liczbacech+1):
         else:
             wynik = 0
     
-        print(wynik)
+    #    print(wynik)
             
         if wynik > maks:
             maks = wynik
             maks_cechy = iterator
     
     print("Wynik: ", maks)
-    print("Najlepsze ", lc, " cechy to: ", maks_cechy)
+    print("Najlepsze cechy to: ", maks_cechy)
     
-    a = maks_cechy
+    t1_stop = perf_counter()
+    print("Elapsed time:", round(t1_stop - t1_start,2), "s")
+    print("Policzone metoda Fisher")
+
+def SFS():
+    a = range(najlepszaCecha,najlepszaCecha+1)
+    b = range(64)
+    prod = product(a,b) #Produkt kartezjanski
     
-    if lc == 2:
-        prod = product(a,a,b)
-        c = list(prod)
-        c = c[64:128]
-        prod = c
-    else:
-        if lc == 3:
-            prod = product(a,a,a,b)
-        if lc == 4:
-            prod = product(a,a,a,a,b)
-        if lc == 5:
-            prod = product(a,a,a,a,a,b)
-        if lc == 6:
-            prod = product(a,a,a,a,a,a,b)
-        if lc == 7:
-            prod = product(a,a,a,a,a,a,a,b)
-        if lc == 8:
-            prod = product(a,a,a,a,a,a,a,a,b)
-        if lc == 9:
-            prod = product(a,a,a,a,a,a,a,a,a,b)
-        if lc == 10:
-            prod = product(a,a,a,a,a,a,a,a,a,a,b)
+    from time import perf_counter
+    t1_start = perf_counter() 
+    
+    for lc in range(2,liczbacech+1):
+         
+        maks = 0
         
-        if lc < liczbacech:
-            c = list(prod)
+        for iterator in prod:
+            print (iterator)
             
-            e = []
-            for d in c:
-                if len(d) == len(set(d)):
-                    e.append(d)
-                    if 63 in d: break
-            prod = e
+            #Pomijaj gdy numer cech sie powtarzaja
+            if len(iterator) != len(set(iterator)):
+                continue
+            
+            licznik = 0
+            
+            macA = []
+            macB = []
+    
+            for i in iterator:
+                licznik = licznik + (pow(sredniaA[i] - sredniaB[i],2))
+                for j in range(176):
+                    macA.append(A[i][j] - sredniaA[i])
+                for j in range(608):
+                    macB.append(B[i][j] - sredniaB[i])
+                licznik = math.sqrt(licznik)
+            
+            macA = np.array(macA)
+            macB = np.array(macB)
+            
+            macA = macA.reshape(lc,176)
+            macB = macB.reshape(lc,608)
+            
+            transA = macA.T
+            resultA =  np.mat(macA) * np.mat(transA)
+            
+            przedDetA = DZIELNIK_A * resultA
+            
+            detA = np.linalg.det(przedDetA)
+        
+            transB = macB.T
+            resultB =  np.mat(macB) * np.mat(transB)
+            
+            przedDetB = DZIELNIK_B * resultB
+            detB = np.linalg.det(przedDetB)
+            
+            mianownik = detA + detB
+        
+            if mianownik != 0:
+                wynik = licznik/mianownik
+            else:
+                wynik = 0
+        
+            print(wynik)
+                
+            if wynik > maks:
+                maks = wynik
+                maks_cechy = iterator
+        
+        print("Wynik: ", maks)
+        print("Najlepsze ", lc, " cechy to: ", maks_cechy)
+        
+        a = maks_cechy
+        
+        if lc == 2:
+            prod = product(a,a,b)
+            c = list(prod)
+            c = c[64:128]
+            prod = c
+        else:
+            if lc == 3:
+                prod = product(a,a,a,b)
+            if lc == 4:
+                prod = product(a,a,a,a,b)
+            if lc == 5:
+                prod = product(a,a,a,a,a,b)
+            if lc == 6:
+                prod = product(a,a,a,a,a,a,b)
+            if lc == 7:
+                prod = product(a,a,a,a,a,a,a,b)
+            if lc == 8:
+                prod = product(a,a,a,a,a,a,a,a,b)
+            if lc == 9:
+                prod = product(a,a,a,a,a,a,a,a,a,b)
+            if lc == 10:
+                prod = product(a,a,a,a,a,a,a,a,a,a,b)
+            
+            if lc < liczbacech:
+                c = list(prod)
+                
+                e = []
+                for d in c:
+                    if len(d) == len(set(d)):
+                        e.append(d)
+                        if 63 in d: break
+                prod = e
+    
+    
+    t1_stop = perf_counter()
+    print("Elapsed time:", round(t1_stop - t1_start,2), "s")
+    print("Policzone metoda SFS")
 
 
-t1_stop = perf_counter()
-print("Elapsed time:", round(t1_stop - t1_start,2), "s")
-print("Policzone metoda SFS")
+if fisher_lub_sfs == 1:
+    Fisher()
 
+if fisher_lub_sfs == 2:
+    SFS()
 
