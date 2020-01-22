2cabc93c71210cc7be862833bea6c9c14145ef10
szymow <39216239+szymow@users.noreply.github.com>
2020-01-22 14:04:16 +0100

    Wczytywanie wsadowe plików z folderu

--- wczytanie_obrazu.py
+++ wczytanie_obrazu.py
@@ -15,20 +15,28 @@ from skimage.io import imread_collection
 from tkinter import filedialog, Label
 import tkinter as tk
 
+import csv
+
 okno = tk.Tk()
 okno.title("SMPD lab Szymon Woyda 227458")
 okno.geometry('640x480')
 etykieta=Label(okno, text=" Wczytaj obraz: ", font=("Arial",16))
 etykieta.grid(column = 0, row = 0)
 
+
+
 def wczytaj():
     global img
+    global jedno
+    jedno = True
     sciezkaDoPliku = filedialog.askopenfilename()
     img = cv2.imread(sciezkaDoPliku,0)
     
 def wczytaj_all():
     global col
-    col_dir = 'img_resize/*.jpg'
+    global jedno
+    jedno = False
+    col_dir = 'img_resize_rename/*.jpg'
     col = imread_collection(col_dir)
     
 
@@ -43,25 +51,45 @@ przycisk_wczytaj_all.grid(column=2, row=1)
 okno.mainloop()
 #https://likegeeks.com/python-gui-examples-tkinter-tutorial/
 
-edges = cv2.Canny(img,100,200)
+if jedno:
+    edges = cv2.Canny(img,100,200)
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(121),plt.imshow(img,cmap = 'gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
-plt.show()
+    plt.show()
 
-unique, counts = np.unique(edges, return_counts=True)
-print(dict(zip(unique, counts)))
+    unique, counts = np.unique(edges, return_counts=True)
+    print(dict(zip(unique, counts)))
+else:
+    export_csv = []
+    i = 0
+    j = 30
+    for reka in col:
+        i = i + 1
+        j = j + 1
+        cv2.imshow(str(i), reka)
+        reka_x = cv2.cvtColor(reka, cv2.COLOR_BGR2GRAY)
+        reka_x = cv2.Canny(reka,100,200)
+        
+        orb = cv2.ORB_create(nfeatures=64)
+        keypoints, descriptors = orb.detectAndCompute(reka_x, None)
+        
+        reka_x = cv2.drawKeypoints(reka_x, keypoints, None)
+        
+        descriptors_mean = descriptors.mean(axis=1)
+        cv2.imshow(str(j), reka_x)
+        
+        export_csv.append(descriptors_mean)
+        
+    b = open("reka.csv", "w")
+    a = csv.writer(b)
+    a.writerows(export_csv)
+    b.close()
 
-reka_1 = col[0]
-reka_1 = cv2.cvtColor(reka_1, cv2.COLOR_BGR2GRAY)
-reka_1 = cv2.Canny(reka_1,100,200)
-
-np.savetxt("foo.csv", reka_1, delimiter=",")
-
-cv2.imshow('image',reka_1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
